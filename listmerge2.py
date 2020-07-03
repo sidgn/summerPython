f29bc402bdd98ff61dfad50113af435235eae3f7
sidgn <67527768+sidgn@users.noreply.github.com>
2020-07-03 19:45:58 -0400

    Add files via upload

--- listmerge2.py
+++ listmerge2.py
@@ -1,17 +1,38 @@
 list2 = [1, 2, 5, 7, 6]
 list1 = [1, 2, 11, 13]
+list3 =[]
 thisdict = dict()
 i = 0
 x1 = 0
-while True:
-    if i >= len(list1) and i >= len(list2): break
-    if i >= len(list1):
-        x1 = 0    
-    elif list1[i] in thisdict: thisdict[list1[i]] += 1
-    else: thisdict[list1[i]] = 1
-    if i >= len(list2):
-        x1 = 0    
-    elif list2[i] in thisdict: thisdict[list2[i]] += 1
-    else: thisdict[list2[i]] = 1
-    i += 1
-print(thisdict.keys())
+def distinct():
+    while True:
+        if i >= len(list1) and i >= len(list2): break
+        if i >= len(list1):
+            x1 = 0    
+        elif list1[i] in thisdict: thisdict[list1[i]] += 1
+        else: thisdict[list1[i]] = 1
+        if i >= len(list2):
+            x1 = 0    
+        elif list2[i] in thisdict: thisdict[list2[i]] += 1
+        else: thisdict[list2[i]] = 1
+        i += 1
+    print(thisdict.keys())
+def common():
+    i = 0
+    while True:
+        if i >= len(list1) and i >= len(list2): break
+        if i >= len(list1):
+            x1 = 0    
+        elif list1[i] in thisdict: 
+            thisdict[list1[i]] += 1
+            list3.append(list1[i])
+        else: thisdict[list1[i]] = 1
+        if i >= len(list2):
+            x1 = 0    
+        elif list2[i] in thisdict:
+            thisdict[list2[i]] += 1
+            list3.append(list1[i])
+        else: thisdict[list2[i]] = 1
+        i += 1
+    print(list3)
+common()
